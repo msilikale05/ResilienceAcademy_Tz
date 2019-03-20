bb4d77e8f6f6880c78d865e16c54479617e1096b
mloweedgar <mloweedgar0@gmail.com>
2019-03-20 15:13:46 +0300

    feat(footer): add site footer

--- footer.php
+++ footer.php
@@ -5,5 +5,46 @@
 <!-- //wrapper -->
 <header class="fixed-menu"></header>
 <?php wp_footer(); ?>
+    <div class="site-footer">
+    <div class="announce-card">to be placed here the checkout content</div>
+    <div class="footer-content">
+        <div class="footer-item">
+            <h4>University Partners</h4>
+            <div  style="border-color: #f9a970" class="footer-divider"></div>
+            <p>
+            University of Dar es salaam <br/>Ardhi University <br/>Sokoine 
+            University of Agriculture<br/> State University of Zanzibar
+            </p>
+        </div>
+
+        <div class="footer-item">
+            <h4>Quick divnks</h4>
+            <div style="border-color: #edd587" class="footer-divider"></div>
+                <p>
+                Tanzania Residivence Academy<br/>Urban Residivence Competence 
+                <br/>Training Modules<br/> News and Events
+                </p>
+        </div>
+        
+        <div class="footer-item">
+            <h4>Connect - Community</h4>
+            <div style="border-color: #f2623a" class="footer-divider"></div>
+                <p>
+                University of Turku, Finland <br/>Delft University 
+                <br/>Ramani Huria<br/>Humanitarian OpenStreetMap Team
+                </p>
+        </div>
+
+        <div class="footer-item">
+            <h4>Follow us</h4>  
+            <div style="border-color: #1729ed" class="footer-divider"></div>
+            <div class="footer-social-media">
+                <div><img src="<?php echo get_bloginfo('template_directory'); ?>/img/icons/facebook-icon.png" alt="facebook" width="30" height="30"/> Facebook</div>
+                <div><img src="<?php echo get_bloginfo('template_directory'); ?>/img/icons/twitter-icon.png" alt="facebook" width="30" height="30"/> Twitter</div>
+                <div><img src="<?php echo get_bloginfo('template_directory'); ?>/img/icons/mobile-icon.png" alt="facebook" width="30" height="25"/> Mobile</div>
+            </div>
+        </div>
+        <div>
+    </div>
 </body>
 </html>
