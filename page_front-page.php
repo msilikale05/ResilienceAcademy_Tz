c3fbd0ad991d9f6ad287c26f6afb5791a55ad69c
mloweedgar <mloweedgar0@gmail.com>
2019-03-26 19:09:08 +0300

    feature: add frontpage layout

---
+++ page_front-page.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Template Name: Front Page
+ */
+get_header(); ?>
+
+<!-- home content -->
+
+<!--Slider-->
+<div class="full_slider preloader">
+    <div id="fullwidth_slider" class="flexslider">
+        <ul class="slides">
+            <li data-thumb="<?php echo get_bloginfo('template_directory'); ?>/dar_from_the_air-thumb.jpg"> <img src="<?php echo get_bloginfo('template_directory'); ?>/img/backgrounds/dar_from_the_air.jpg" alt="" class="slide_bg" /></li>
+            <li data-thumb="bg_1-thumb.jpg"> <img src="<?php echo get_bloginfo('template_directory'); ?>/img/bg_1.jpg" alt="" class="slide_bg" /></li>
+        </ul>
+    </div>
+</div>
+
+<!--//Slider-->
+<section class="ftco-section ftco-no-pb ftco-no-pt">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="search-wrap-1 ftco-animate p-4">
+                    <nav id="gtl-site-navigation" class="middle_nav">
+                        <div class="" id="middle_nav">
+                            <?php 
+                            wp_nav_menu(array(
+                                'theme_location' => 'tertiary',
+                                'menu_id'         => 'middle_nav',
+                                'menu_class'      => 'tertiary-items',
+                                'container-fluid'      => 'div',
+                                'container_class' => 'tertiary_menu',
+                                'walker'         => '',
+                                'fallback_cb'    => 'wp_page_menu',
+
+                            ));
+                            ?>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="bg-image"></div>
+    <?php while (have_posts()) : the_post(); ?>
+    <?php the_content(); ?>
+    <?php endwhile; ?>
+</section>
+<section class="latest">
+    <div class="container" style="padding-top:44px; ">
+        <h2 style="text-align: center;"> Recent News for resilience academy</h2>
+        <div class="row">
+            <?php
+            $args = array('numberposts' => 3, 'order' => 'DSC', 'orderby' => 'title');
+            $postslist = get_posts($args);
+            $max_size = 12 / sizeof($postslist);
+            $col_css = "col-md-" . $max_size;
+            foreach ($postslist as $post) :  setup_postdata($post); ?>
+            <div class="<?php echo $col_css ?>">
+                <div class="card">
+                    <figure class="blogimg">
+                        <?php the_post_thumbnail('single-featured'); ?>
+                    </figure>
+                    <div class="blog-title">
+                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <div class="auth">
+                            <h6>Written by <?php the_author(); ?>
+                                <time class="postdate"><?php echo get_the_date(); ?></time>
+                            </h6>
+                        </div>
+                        <div class="blog-info">
+                            <p><?php the_excerpt(); ?></p>
+                            <a href="<?php the_permalink(); ?>" class="blog-link">
+                                <?php esc_attr_e('Read More ', 'wp_theme_foss_4_g_dar'); ?>
+                                <i class="fa fa-long-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</section>
+
+</div>
+</div>
+<?php get_footer(); ?> 
