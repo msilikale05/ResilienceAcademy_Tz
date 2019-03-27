66fb833a467e74f094a14edef0e90b38a196fc8b
mkumbobeaty <charsbeaty@gmail.com>
2019-03-27 18:57:23 +0300

    add middle menu in resources

--- research.php
+++ research.php
@@ -77,4 +77,30 @@ get_header();  ?>
         </div>
     </div>
 </div>
+<section class="ftco-section ftco-no-pb ftco-no-pt">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="search-wrap-2 ftco-animate p-4">
+                    <nav id="gtl-site-navigation" class="middle_nav">
+                        <div class="" id="middle_nav">
+                            <?php 
+                            wp_nav_menu(array(
+                              'theme_location' => 'tertiary',
+                              'menu_id'         => 'middle_nav',
+                              'menu_class'      => 'tertiary-items',
+                              'container-fluid'      => 'div',
+                              'container_class' => 'tertiary_menu',
+                              'walker'         => '',
+                              'fallback_cb'    => 'wp_page_menu',
+
+                            ));
+                            ?>
+                        </div>
+                    </nav>
+		        </div>
+			</div>
+	    	</div>
+	    </div>
+    </section>
 <?php get_footer(); ?> 
