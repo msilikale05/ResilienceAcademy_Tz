a281e4c34b24ab8f9add6b5148e4a00c6f015bf4
mkumbobeaty <charsbeaty@gmail.com>
2019-04-01 23:10:21 +0300

    implement page for displaying blog details

--- single.php
+++ single.php
@@ -4,12 +4,47 @@
   *
  * 
  */
+get_header();?>
+<section class="hero hero_inner hero_inner-article">
 
-get_header(); ?>
+<!-- hero__layout -->
+<div class="hero__layout">
 
-<h1>Comming Soon!</h1>
-<p>This page is still under construction will be up soon...</p>
+    <!-- site__title -->
+    <h2 class="site__title site__title_inner"><?php the_title(); ?></h2>
+    <!-- /site__title -->
 
-<?php 
+    <!--article-info-->
+    <div class="article-info">
+
+        <time class="article-info__item" datetime="<?php echo get_the_date(); ?>"><?php echo get_the_date(); ?></time>
+
+    </div>
+    <!--/article-info-->
+
+</div>
+<!-- /hero__layout -->
+
+</section>
+<?php do_action('wp_theme_foss_4_g_dar_action_breadcrumb'); ?>
+<div class="cart-section-lst section-padding white-bg">
+        <section class=" blog-header">
+            <div>
+             <div class="section-title text-center">
+                 <h2 id="blog-title"><?php single_post_title(); ?></h2>
+             </div>
+            </div>
+        </section>
+    <div class="container">				
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'content', 'single' ); ?>
+
+		<?php endwhile; // end of the loop. ?>
+	</div>
+</div>
+ <?php 
 get_footer();
-?> 
+?>
+
+<?php
